cadb6bb11c6420cbbffdd066405ce48a03bfac39
Luís Kennedy <62557726+KancsSneed@users.noreply.github.com>
2020-11-03 09:36:35 -0300

    estudando o kivy

--- code/desenvolvimento_a_parte/interface/main.py
+++ code/desenvolvimento_a_parte/interface/main.py
@@ -1,15 +1,11 @@
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 
-class TestApp(App):
+class Adicionador(BoxLayout):
+    pass
+
+class Interface(App):
     def build(self):
-        box = BoxLayout(orientation='vertical')
-        button1 = Button(text='Button1')
-        label1 = Label(text='Label 1')
-        box.add_widget(button1)
-        box.add_widget(label1)
-        return box
+        return Adicionador()
 
-TestApp().run()
+Interface().run()
