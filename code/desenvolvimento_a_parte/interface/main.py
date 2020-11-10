ea80d9927941b78ed5c1b5598890e721d19ca9e1
Luís Kennedy <62557726+KancsSneed@users.noreply.github.com>
2020-11-10 19:49:48 -0300

    Estudando...

--- code/desenvolvimento_a_parte/interface/main.py
+++ code/desenvolvimento_a_parte/interface/main.py
@@ -1,11 +1,18 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 
-class Adicionador(BoxLayout):
-    pass
-
+class Tarefas(ScrollView):
+    def __init__(self, tarefas,**kwargs):
+        super().__init__(**kwargs)
+        for tarefa in tarefas:
+            self.ids.box.add_widget(Tarefa(text=tarefa))
+class Tarefa(BoxLayout):
+    def __init__(self,tarefas,text='', **kwargs):
+        super().__init__(**kwargs)
+        self.ids.label.text = text
 class Interface(App):
     def build(self):
-        return Adicionador()
-
+        return Tarefas(['Atividade','Fazer compras', 'Buscar o filho','Atividade','Fazer compras', 'Buscar o filho','Atividade','Fazer compras', 'Buscar o filho'])
 Interface().run()
