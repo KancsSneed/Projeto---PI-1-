2e200d3f38eb63d2ae29e00fb8186456959b82f6
Luís Kennedy <62557726+KancsSneed@users.noreply.github.com>
2020-12-20 16:43:01 -0300

    Finalizado

---
+++ Mec_study-versions/Betas/1.0.0/code/App/main/main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+class main(App):
+    def build(self):
+        box = BoxLayout(orientation='vertical')
+        button1 = Button(text='Button1')
+        label1 = Label(text='Label 1')
+        box.add_widget(button1)
+        box.add_widget(label1)
+        return box
+
+main().run()
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Projeto-MecStudy
+
+Intergrantes:
+
+    Antonio Neto
+    João Gilbert
+    Kauan Rameh
+    Luís Kennedy
+    Gabriel Miranda
+
+Links:
+
+    Marvel - https://marvelapp.com/prototype/bd37aj1
+
+    Sistema de login - https://youtu.be/e_Uk1tXEChU
+
